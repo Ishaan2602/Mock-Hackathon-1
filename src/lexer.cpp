cfa36c718d0ee6e3a14a8017eefdf9d7b36359bf
Anirudh Konidala <kidskodingclub@gmail.com>
2025-01-26 19:08:14 -0600

    Basic python interpreter executor in CLI

---
+++ src/lexer.cpp
@@ -0,0 +1,17 @@
+enum class TokenType {
+    IDENTIFIER,   
+    NUMBER,       
+    STRING,
+    NEWLINE,
+    INDENT,
+    DEDENT,
+    OPERATOR,
+    KEYWORD,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+    COLON,
+    COMMA,
+    EOF_TOKEN
+};
