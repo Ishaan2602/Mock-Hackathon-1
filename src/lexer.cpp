0c1069ec9ff8dedb845afc91fcc191c15866d4ed
GodlyCarrot <robertlquan3@gmail.com>
2025-01-26 19:53:31 -0600

    added splitting for lexer

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -7,13 +7,22 @@ std::vector<std::string> split(std::string sourceCode) {
     std::vector<std::string> lines;
     std::string line;
 
+    int len = sourceCode.length();
+
+    for (int i = len; i >= 0; i--) {
+        if (sourceCode[i] == ' ') {
+            lines.push_back(line);
+            line = "";
+        } else {
+            line += sourceCode[i];
+        }
+    }
     return lines;
 };
 
 std::vector<Token> totoken(std::string sourceCode) {
     std::vector<Token> tokens;
-
-    
+    std::vector<std::string> lines = split(sourceCode);
 
     return tokens;
 };
